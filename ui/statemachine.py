2aced78bff039f7eaffb5df221a93ef447dc891a
Paul G. <pglotfel@ycp.edu>
2014-08-06 21:37:04 -0400

    More changes to structure

--- ui/statemachine.py
+++ ui/statemachine.py
@@ -40,8 +40,8 @@ def mark_received(input_vector):
         input_vector[i] = 0;
 
 class State:
-    def __init__(self, id, function, next_state):
-        self.id = id
+    def __init__(self, number, function, next_state):
+        self.id = number
         self.function = function
         self.next_state = next_state
         
@@ -53,7 +53,7 @@ class State:
             #Case when no input...
             self.function();
             if(type(self.next_state) is list):
-                return self.id
+                return self.number
             else:
                 return self.next_state
         
@@ -76,96 +76,34 @@ def generateInputVector(num_inputs):
         ret.append(0)
         
     return ret
-    
-def state_one(input_vector):
-    
-    print('Report a problem!')
-    
-    ret = 1;
-    
-    if(input_vector[5]):
-        ret = 0
-    elif(input_vector[0]):
-        ret = 3
-    elif(input_vector[1]):
-        ret = 4
-    elif(input_vector[2]):
-        ret = 5
-    elif(input_vector[3]):
-        ret = 6
-    elif(input_vector[4]):
-        ret = 7
-      
-    mark_received(input_vector) 
-    return ret;
 
-def state_three(input_vector):
+def state_eight():
+    print('thank for reporting a problem')
     
-    print('Chain broken report')
-    
-    return 8
+    time.sleep(5)
 
-def state_four(input_vector):
-    
-    print('Seat damaged')
-    
-    return 8
-
-def state_five(input_vector):
-    
-    print('flat tire')
-    
-    return 8
-
-def state_six(input_vector):
-    
-    print('Missing basket')
-    
-    return 8
-
-def state_seven(input_vector):
-    
-    print('Other')
-    
-    return 8
-
-def state_eight(input_vector):
-    
-    print('Thanks for reporting a problem')
-    
-    time.sleep(2)
-      
-    return 0
-    
-#Intialize state dictionary
-
-#NEW STATES! :D Much fancier!
-
-   
-states = {0 : State(0, lambda: print('state_zero'), [0, 1, 2, 0, 0, 0]),
-          1 : State(1, lambda: print('report a problem'), [3, 4, 5, 6, 7, 0]), 
-          2 : State(2, lambda: print('view information '), [0, 0, 0, 0, 0, 0]), 
+states = {0 : State(0, lambda: print('state_zero'), [0, 1, 2, 6]),
+          1 : State(1, lambda: print('report a problem'), [3, 4, 5, 0]), 
+          2 : State(2, lambda: print('view information '), [0, 0, 0, 0]), 
           3 : State(3, lambda: print('Chain broken report'), 8),
           4 : State(4, lambda: print('Seat damaged'), 8),
           5 : State(5, lambda: print('Flat tire'), 8),
-          6 : State(6, lambda: print('Missing basket'), 8),
-          7 : State(7, lambda: print('Other'), 8),
+          6 : State(6, lambda: print('Rent a bike'), 0),
           8 : State(8, state_eight, 0)}
 
-print(states.get(0).run([0, 0, 0, 0, 0, 0]))
-
-print(states.get(3).run([0, 0, 0, 0, 0, 0]))
-
-print(states.get(3).run([0, 1]));
+# print(states.get(0).run([0, 0, 0, 0, 0, 0]))
+# 
+# print(states.get(3).run([0, 0, 0, 0, 0, 0]))
+# 
+# print(states.get(3).run([0, 1]));
 
 current_state = 0
-system_input = generateInputVector(5)
-
+system_input = generateInputVector(4)
 
 #Try making a test that has a 'forward' and a 'back' button.  Forward goes to some sort of information?  Maybe a map?
 
 #while(1):
-#   current_state = states.get(current_state).run(input_vector)
+#  current_state = states.get(current_state).run(input_vector)
 #  time.sleep(1)
 
 
